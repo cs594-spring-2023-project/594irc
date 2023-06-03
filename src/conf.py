0c203198706f937dd9a96195c6887d432b4dd82e
Amelia Miner <ameliamminer@gmail.com>
2023-06-02 18:32:59 -0700

    close_on_err now sends a packet, added naive bytestring construction methods, as well as some validation stuff

--- src/conf.py
+++ src/conf.py
@@ -11,6 +11,7 @@ TIMEOUT = 5
 # IRC version
 IRC_VERSION = 0x1337
 
+IRC_COMMAND_VALUES = [i for i in range(0x00, 0x0B)] # for validation
 # IRC commands ~ client or server
 IRC_ERR = 0x00
 IRC_KEEPALIVE = 0x01
@@ -26,6 +27,7 @@ IRC_LISTROOMS_RESP = 0x08
 IRC_LISTUSERS_RESP = 0x09
 IRC_TELLMSG = 0x0A
 
+IRC_ERR_VALUES = [i for i in range(0x10, 0x19)] # for validation
 # IRC error codes
 IRC_ERR_UNKNOWN = 0x10
 IRC_ERR_ILLEGAL_OPCODE = 0x11
@@ -37,7 +39,6 @@ IRC_ERR_NAME_EXISTS = 0x16
 IRC_ERR_TOO_MANY_USERS = 0x17
 IRC_ERR_TOO_MANY_ROOMS = 0x18
 
-# generic message format
 class IrcHeader:
     ''' holds the header of an IRC message
     '   opcode: IRC command
@@ -47,6 +48,13 @@ class IrcHeader:
     def __init__(self, opcode, length):
         self.opcode = opcode # should be 1 byte
         self.length = length # should be 4 bytes
+    
+    def construct_header(self):
+        ''' returns a byte representation of the header '''
+        opcode_bytes = self.header.opcode.to_bytes(1, 'big')
+        length_bytes = self.header.length.to_bytes(4, 'big')
+        return opcode_bytes + length_bytes
+
 
 class IrcPacketHello:
     ''' has a header, holds the body of an IRC hello message
@@ -59,6 +67,18 @@ class IrcPacketHello:
         self.payload = username # should be 32 bytes
         self.version = version # should be 2 bytes
 
+    def construct_packet(self):
+        ''' returns a byte representation of the packet '''
+        # validate fields
+        if not validate_label(self.payload):
+            raise ValueError(f"Invalid username: {self.payload}")
+        # construct and return bytestring
+        header_bytes = self.header.construct_header()
+        payload_bytes = label_to_bytes(self.payload)
+        version_bytes = self.version.to_bytes('big')
+        return header_bytes + payload_bytes + version_bytes
+        
+
 class IrcPacketMsg:
     ''' has a header, holds the body of an IRC message
     '   header: irc_header object
@@ -67,6 +87,19 @@ class IrcPacketMsg:
     def __init__(self, opcode, payload):
         self.header = IrcHeader(opcode, len(payload))
         self.payload = payload
+
+    def construct_packet(self):
+        ''' returns a byte representation of the packet '''
+        # validate fields
+        if self.header.opcode not in IRC_COMMAND_VALUES:
+            raise ValueError(f"Invalid opcode: {self.header.opcode}")
+        if self.header.length != len(self.payload):
+            raise ValueError(f"Invalid length: {self.header.length}")
+        # construct and return bytestring
+        header_bytes = self.header.construct_header()
+        payload_bytes = self.payload.encode('ascii')
+        return header_bytes + payload_bytes
+
 
 class IrcPktErr:
     ''' has a header, holds the body of an IRC error message
@@ -77,6 +110,17 @@ class IrcPktErr:
         self.header = IrcHeader(IRC_ERR, 1)
         self.payload = payload # should be an IRC_ERR_* code, 1 byte
 
+    def construct_packet(self):
+        # validate fields
+        if self.payload not in IRC_ERR_VALUES:
+            raise ValueError(f"Invalid error code: {self.payload}")
+        # construct and return bytestring
+        opcode_bytes = self.header.opcode.to_bytes(1, 'big')
+        length_bytes = self.header.length.to_bytes(4, 'big')
+        payload_bytes = self.payload.to_bytes(1, 'big')
+        return opcode_bytes + length_bytes + payload_bytes
+
+
 class IrcPktKeepalive:
     ''' has a header, holds the body of an IRC keepalive message
     '   header: irc_header object
@@ -85,12 +129,23 @@ class IrcPktKeepalive:
     def __init__(self):
         self.header = IrcHeader(IRC_KEEPALIVE, 0)
 
-def close_on_err(sock, err):
+    def construct_packet(self):
+        ''' returns a byte representation of the packet '''
+        # validate fields
+        if self.header.opcode != IRC_KEEPALIVE:
+            raise ValueError(f"Invalid opcode: {self.header.opcode}")
+        # construct and return bytestring
+        header_bytes = self.header.construct_header()
+        return header_bytes
+
+def close_on_err(sock, err_code, err_msg=None):
     ''' closes a socket and prints an error message
     '   sock: socket to close
-    '   err: error message to print
+    '   err_code: error code to send
+    '   err_msg: error message to print
     '''
-    print(err)
+    print(err_msg)
+    sock.send(IrcPktErr(err_code).construct_packet())
     sock.close()
     sys.exit(1)
 
@@ -115,3 +170,10 @@ def validate_label(label):
     '   returns: True if valid, False otherwise
     '''
     return len(label) <= 32 and validate_string(label)
+
+def label_to_bytes(label):
+    ''' converts a label to 32 byte null-padded bstring
+    '   label: label to convert
+    '   returns: byte representation of label
+    '''
+    return label.encode('ascii').ljust(32, b'\x00')
